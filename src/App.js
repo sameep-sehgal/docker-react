ca6704a15d3443b7ad849a79dbc7756c7436012d
Sameep <sameepsam34@gmail.com>
2020-09-05 10:25:30 +0530

    Changed text feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Eit <code>src/App.js</code> and save to reload.
+          Edited this on feature branch.
         </p>
         <a
           className="App-link"
